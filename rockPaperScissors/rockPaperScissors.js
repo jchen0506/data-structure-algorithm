b34ca9bafd5ed2b5756fcd4e40a23baf4c5c93e4
Jing Chen <jchen0506@gmail.com>
2020-05-19 11:35:06 -0500

    Finished bare minimum
    generates every sequence of throws a single player could throw over a three-round game of rock-paper-scissors.

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,25 +1,51 @@
 /*
-* Write a function that generates every sequence of throws a single
-* player could throw over a three-round game of rock-paper-scissors.
-*
-* Your output should look something like:
-*   ["RRR",
-*    "RRP",
-*    "RRS",
-*    "RPR",
-*    ...etc...
-*   ]
-*
-* Extra credit:
-*   - Make your function return answers for any number of rounds.
-*
-* Example:
-* rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
-*
-*/
+ * Write a function that generates every sequence of throws a single
+ * player could throw over a three-round game of rock-paper-scissors.
+ *
+ * Your output should look something like:
+ *   ["RRR",
+ *    "RRP",
+ *    "RRS",
+ *    "RPR",
+ *    ...etc...
+ *   ]
+ *
+ * Extra credit:
+ *   - Make your function return answers for any number of rounds.
+ *
+ * Example:
+ * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
+ *
+ */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
   // TODO: your solution here
+  var finalresult = [];
+  // create a element container
+  var container = ['R', 'P', 'S'];
+  // 3 layers loop, iterate the elment
+  // first layer, first round
+  for (var i = 0; i < container.length; i++) {
+    var oneresult = [];
+    var first = container[i];
+    oneresult.push(first);
+    //2nd layer, 2nd round
+    for (var j = 0; j < container.length; j++) {
+      var second = container[j];
+      oneresult.push(second);
+      //3nd layer, 3rd round
+      for (var k = 0; k < container.length; k++) {
+        var third = container[k];
+        oneresult.push(third);
+        var tempString = oneresult.join('');
+        oneresult.pop();
+        finalresult.push(tempString);
+      }
+      oneresult.pop();
+    }
+    oneresult.pop();
+  }
+  return finalresult;
 };
 
+console.log(rockPaperScissors());
