0100dacd8bf81e92fd645b77aa78e45c48f6f84f
Jing Chen <jchen0506@gmail.com>
2020-05-28 23:33:57 -0500

    Finished Quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -4,56 +4,55 @@
  * It:
  *    Takes in an array.
  *    [5,1,4,3,6,10,2]
+ *   low          high
+ *                pivot
  *    Picks a value in the array as a pivot.
  *    [5,1,4,3,6,10,2]
  *     |
  *    Partitions all the elements of the array into two arrays, based on
  *      if they are larger or smaller than the pivot.
  *    [5,1,4,3,2,6,10]
+
  *    [5,1,4,3,2,6,10]
  *    Recursively sorts the two halves.
  *    Combines the two arrays and the pivot into a sorted array.
  */
 // O(logn)
 var quicksort = function (array) {
-  var left = 0;
-  var right = array.length - 1;
-  if (left < right) {
-    //using partition helper function to update the pivot position
-    pivot = partition(array, left, right);
-    // seperate the original array to two parts while left is smaller than pivot right is greater than pivot
-    arrayleft = array.slice(left, pivot - 1);
-    arrayright = array.slice(pivot + 1, right);
-    //recursively sort both part
-    quicksort(arrayleft);
-    quicksort(arrayright);
-  }
+  var low = 0;
+  var high = array.length - 1;
+  var sort = function (array, low, high) {
+    if (low < high) {
+      var pivot = partition(array, low, high);
+      sort(array, low, pivot - 1);
+      sort(array, pivot + 1, high);
+    }
+  };
+  sort(array, low, high);
   return array;
 };
 // find where the pivot should go
-var partition = function (array, left, right) {
-  var pivot = array[right]; // pivot is alway the last element of array
-  while (left < right) {
-    //move the element greater than pivot to the right until it hits the element that is smaller than pivot
-    while (array[left] > array[pivot]) {
-      swap(left, left + 1, array);
-      left++;
-    }
-    //move the element smaller than pivot to the right until it hits the element that is greater than pivot
-    while (array[right] < array[pivot]) {
-      swap(right - 1, right, array);
-      right--;
+var partition = function (array, low, high) {
+  var pivot = array[high]; // pivot is alway the last element of array
+  var i = low - 1;
+  for (var j = low; j < high; j++) {
+    if (array[j] < pivot) {
+      i++;
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
   }
-  pivot = (right + left) / 2;
-  return pivot;
+  var temp2 = array[i + 1];
+  array[i + 1] = array[high];
+  array[high] = temp2;
+  return i + 1;
 };
 
 var swap = function (first, second, array) {
   var temp = array[first];
   array[first] = array[second];
   array[second] = temp;
-  return array;
 };
 
 console.log(quicksort([5, 1, 4, 3, 6, 2, 10]));
