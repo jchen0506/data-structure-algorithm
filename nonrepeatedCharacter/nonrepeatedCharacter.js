b3ee83d9ab65dfa716151bc77ebba856ba6d2f38
Jing Chen <jchen0506@gmail.com>
2020-06-02 11:21:01 -0500

    Finished nonrepeatedChar

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,27 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
+var firstNonRepeatedCharacter = function (string) {
   // TODO: your solution here
+  var array = string.split('');
+  var occurance = {};
+  for (var i = 0; i < array.length; i++) {
+    let current = array[i];
+    occurance[current] =
+      occurance[current] === undefined ? 1 : occurance[current] + 1;
+  }
+
+  for (var j = 0; j < array.length; j++) {
+    let current = array[j];
+    if (occurance[current] === 1) {
+      return current;
+    }
+  }
+  return null;
 };
+
+var string = 'AABBCCCDEB';
+var string2 = '2AAAbbbb111';
+
+console.log(firstNonRepeatedCharacter(string));
+console.log(firstNonRepeatedCharacter(string2));
