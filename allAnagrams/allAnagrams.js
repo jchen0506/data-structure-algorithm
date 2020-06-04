6b2e1d5eaa2ad716f3b11ad145d7c517b04ab935
Jing Chen <jchen0506@gmail.com>
2020-06-04 12:04:54 -0500

    Attemped

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,15 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+var allAnagrams = function (string) {
   // Your code here.
+  var result = [];
+  var newstring = string + string;
+  var array = newstring.split('');
+  for (var i = 0; i < newstring.length - string.length + 1; i += string.length) {
+    result.push(array.splice(i, string.length).join(''))
+  }
+  return result;
 };
+
+console.log(allAnagrams('abc'));
