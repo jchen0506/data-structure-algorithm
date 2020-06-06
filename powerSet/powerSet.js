d5116e8aadcec7fbce4c75e29d5e725ef5798d79
Jing Chen <jchen0506@gmail.com>
2020-06-06 11:37:52 -0500

    First attemp of powerSet

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,39 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
+// jump => jmpu =>
+//    ==>j, jm,jp, ju,jmp,jmu, jpu,jmpu
+//    ==>m, mp, mu, mpu
 
 var powerSet = function(str) {
+  var array = str.split('').sort();
+  var result =[];
+  for (var i=0; i < str.length; i++){
+    result.push(['',array[i]]);
+  }
+  // console.log(result)
+  for ( var j=result.length-1;j>0;j--){
+    // result[j].push(array[j]);
+    var last =result[j];
+    var first= result[j-1];
+    // console.log(last);
+    // console.log(first);
+    for (var k =1; k<last.length;k++){
+      first.push( first[0].concat(last[k]))
+      first.push( first[1].concat(last[k]));
+    }
+  }
+  return result[0];
+
 };
+
+console.log(powerSet('abc'));
