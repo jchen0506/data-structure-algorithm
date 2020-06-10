b4709d388fab217a4c4fc9316703d64b4e53a365
Jing Chen <jchen0506@gmail.com>
2020-06-10 11:23:41 -0500

    Finished binarySearch with recursion

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,26 @@
  */
 
 var binarySearch = function (array, target) {
+  var result = 0;
+  var helper = function (left, right, array) {
+    var mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else {
+      while (left < right) {
+        if (array[mid] > target) {
+          return helper(left, mid - 1, array);
+        } else {
+          return helper(mid + 1, right, array);
+        }
+      }
+    }
+    return null;
+  };
+  result = helper(0, array.length - 1, array);
+  return result;
 };
 
+var array = [1, 2, 3, 4, 5];
+console.log(binarySearch(array, 4));
+console.log(binarySearch(array, 6));
