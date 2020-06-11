9ee548f4e17a5def1898192dcb0016d8e07c2de3
Jing Chen <jchen0506@gmail.com>
2020-06-11 11:24:55 -0500

    First attempt to countLeave

--- treeCountLeaves/treeCountLeaves.js
+++ treeCountLeaves/treeCountLeaves.js
@@ -1,55 +1,66 @@
-
 /**
-  * Implement the `countLeaves` function in this Tree class.
-  *
-  * A leaf node is any node in the tree that has no children. `countLeaves` should
-  * traverse the tree, and return the number of leaf nodes the tree contains.
-  *
-  * Illustration of a tree with three leaves:
-  *
-  *       * <- root
-  *      / \
-  *     *    * <- leaf
-  *    / \
-  *   *   * <- leaf
-  *  /
-  * * <- leaf
-  *
-  * Example usage:
-  *   var root = new Tree();
-  *   root.countLeaves(); // 1
-  *   root.addChild(new Tree());
-  *   root.countLeaves(); // still 1
-  *   root.addChild(new Tree());
-  *   root.children[0].addChild(new Tree());
-  *   root.children[0].addChild(new Tree());
-  *   root.children[0].children[0].addChild(new Tree());
-  *   root.countLeaves(); // 3
-  *
-  */
+ * Implement the `countLeaves` function in this Tree class.
+ *
+ * A leaf node is any node in the tree that has no children. `countLeaves` should
+ * traverse the tree, and return the number of leaf nodes the tree contains.
+ *
+ * Illustration of a tree with three leaves:
+ *
+ *       * <- root
+ *      / \
+ *     *    * <- leaf
+ *    / \
+ *   *   * <- leaf
+ *  /
+ * * <- leaf
+ *
+ * Example usage:
+ *   var root = new Tree();
+ *   root.countLeaves(); // 1
+ *   root.addChild(new Tree());
+ *   root.countLeaves(); // still 1
+ *   root.addChild(new Tree());
+ *   root.children[0].addChild(new Tree());
+ *   root.children[0].addChild(new Tree());
+ *   root.children[0].children[0].addChild(new Tree());
+ *   root.countLeaves(); // 3
+ *
+ */
 
 /*
  * Basic tree that stores a value.
  */
 
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 };
 
 Tree.prototype.countLeaves = function () {
   // TODO: implement me!
+  var count = 0;
+  var helper = function (node) {
+    if (node.children.length === 0) {
+      count++;
+    } else {
+      for (var i = 0; i < node.children.length; i++) {
+        helper(node.children[i]);
+      }
+    }
+  };
+  helper(this);
+  return count;
 };
 
 /**
-  * You shouldn't need to change anything below here, but feel free to look.
-  */
+ * You shouldn't need to change anything below here, but feel free to look.
+ */
 
 /**
-  * add an immediate child
-  * (wrap values in Tree nodes if they're not already)
-  */
-Tree.prototype.addChild = function(child) {
+ * add an immediate child
+ * (wrap values in Tree nodes if they're not already)
+ */
+Tree.prototype.addChild = function (child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
@@ -64,10 +75,10 @@ Tree.prototype.addChild = function(child) {
 };
 
 /**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-Tree.prototype.isDescendant = function(child) {
+ * check to see if the provided tree is already a child of this
+ * tree __or any of its sub trees__
+ */
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -83,9 +94,9 @@ Tree.prototype.isDescendant = function(child) {
 };
 
 /**
-  * remove an immediate child
-  */
-Tree.prototype.removeChild = function(child) {
+ * remove an immediate child
+ */
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
@@ -94,3 +105,13 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+var root = new Tree();
+console.log(root.countLeaves()); //1
+root.addChild(new Tree());
+console.log(root.countLeaves()); // still 1
+root.addChild(new Tree());
+root.children[0].addChild(new Tree());
+root.children[0].addChild(new Tree());
+root.children[0].children[0].addChild(new Tree());
+console.log(root.countLeaves()); // 3
