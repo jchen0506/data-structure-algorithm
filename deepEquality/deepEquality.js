c6a2ad82f409e8d53737ad6b26357735718286e4
Jing Chen <jchen0506@gmail.com>
2020-06-12 11:43:18 -0500

    First attempt to deepEquals

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -1,15 +1,62 @@
 /**
-  * Write a function that, given two objects, returns whether or not the two
-  * are deeply equivalent--meaning the structure of the two objects is the
-  * same, and so is the structure of each of their corresponding descendants.
-  *
-  * Examples:
-  *
-  * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
-  * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
-  *
-  * don't worry about handling cyclical object structures.
-  *
-  */
-var deepEquals = function(apple, orange) {
+ * Write a function that, given two objects, returns whether or not the two
+ * are deeply equivalent--meaning the structure of the two objects is the
+ * same, and so is the structure of each of their corresponding descendants.
+ *
+ * Examples:
+ *
+ * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+ * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+ *
+ * don't worry about handling cyclical object structures.
+ *
+ */
+var deepEquals = function (apple, orange) {
+  var appleKey = Object.keys(apple);
+  var orangeKey = Object.keys(orange);
+  var isDeepEqual = true;
+
+  if (appleKey.length !== orangeKey.length) {
+    isDeepEqual = false;
+  } else {
+    for (var i = 0; i < appleKey.length; i++) {
+      if (appleKey[i] !== orangeKey[i]) {
+        isDeepEqual = false;
+      } else if (
+        (typeof apple[appleKey[i]] === 'object' &&
+          typeof orange[orangeKey[i]] !== 'object') ||
+        (typeof apple[appleKey[i]] !== 'object' &&
+          typeof orange[orangeKey[i]] === 'object')
+      ) {
+        return false;
+      } else if (
+        typeof apple[appleKey[i]] !== 'object' &&
+        typeof orange[orangeKey[i]] !== 'object' &&
+        apple[appleKey[i]] !== orange[orangeKey[i]]
+      ) {
+        isDeepEqual = false;
+      } else {
+        isDeepEqual = deepEquals(apple[appleKey[i]], orange[orangeKey[i]]);
+      }
+    }
+    return isDeepEqual;
+  }
 };
+
+/*
+{
+  a: 1,
+  b: {
+    c: 3
+  }
+}
+
+{
+  a: 1,
+  b: {
+    c: 3
+  }
+}
+*/
+console.log(deepEquals({ a: 1, b: { c: 3 } }, { a: 1, b: { c: { d: 5 } } }));
+console.log(deepEquals({ a: 1, b: { c: 5 } }, { a: 1, b: { c: 5 } }));
