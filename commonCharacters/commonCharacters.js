261530111d85979883febbe093980939846ece7e
Jing Chen <jchen0506@gmail.com>
2020-06-19 11:30:57 -0500

    commonChar finished

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,8 +10,29 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
-
-
-var commonCharacters = function(string1, string2) {
+var commonCharacters = function (string1, string2) {
   // TODO: Your code here!
+  var result = [];
+  var obj1 = {};
+  var obj2 = {};
+  for (let i = 0; i < string1.length; i++) {
+    let char = string1[i];
+    obj1[char] = obj1[char] === undefined ? 1 : obj1[char] + 1;
+  }
+  for (let i = 0; i < string2.length; i++) {
+    let char = string2[i];
+    obj2[char] = obj2[char] === undefined ? 1 : obj2[char]++;
+  }
+  console.log(obj1);
+  for (let i = 0; i < string1.length; i++) {
+    let char = string1[i];
+    if (obj2[char] !== undefined && result.indexOf(char) === -1) {
+      result.push(char);
+    }
+  }
+  return result.join('');
 };
+
+//test
+var result = commonCharacters('aeexivou', 'aegeihobu');
+console.log(result);
