1cca8efcc6b00ce841e6b130327f4e3134f1ba3a
Jing Chen <jchen0506@gmail.com>
2020-06-19 11:53:36 -0500

    Finished extra credit for commonChars

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,29 +10,61 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
-var commonCharacters = function (string1, string2) {
-  // TODO: Your code here!
+// var commonCharacters = function (string1, string2) {
+//   // TODO: Your code here!
+//   var result = [];
+//   var obj1 = {};
+//   var obj2 = {};
+//   for (let i = 0; i < string1.length; i++) {
+//     let char = string1[i];
+//     obj1[char] = obj1[char] === undefined ? 1 : obj1[char] + 1;
+//   }
+//   for (let i = 0; i < string2.length; i++) {
+//     let char = string2[i];
+//     obj2[char] = obj2[char] === undefined ? 1 : obj2[char]++;
+//   }
+//   console.log(obj1);
+//   for (let i = 0; i < string1.length; i++) {
+//     let char = string1[i];
+//     if (obj2[char] !== undefined && result.indexOf(char) === -1) {
+//       result.push(char);
+//     }
+//   }
+//   return result.join('');
+// };
+
+//test
+// var result = commonCharacters('aeexivou', 'aegeihobu');
+// console.log(result);
+
+var commonCharacters = function () {
+  var args = Array.prototype.slice.call(arguments);
+  var objArray = [];
   var result = [];
-  var obj1 = {};
-  var obj2 = {};
-  for (let i = 0; i < string1.length; i++) {
-    let char = string1[i];
-    obj1[char] = obj1[char] === undefined ? 1 : obj1[char] + 1;
+  for (let i = 0; i < args.length; i++) {
+    var string = args[i];
+    var obj = {};
+    for (let j = 0; j < string.length; j++) {
+      let char = string[j];
+      obj[char] = 1;
+    }
+    objArray.push(obj);
   }
-  for (let i = 0; i < string2.length; i++) {
-    let char = string2[i];
-    obj2[char] = obj2[char] === undefined ? 1 : obj2[char]++;
-  }
-  console.log(obj1);
-  for (let i = 0; i < string1.length; i++) {
-    let char = string1[i];
-    if (obj2[char] !== undefined && result.indexOf(char) === -1) {
+  console.log(objArray);
+  for (let i = 0; i < args[0].length; i++) {
+    let char = args[0][i];
+    var flag = true;
+
+    for (let j = 1; j < objArray.length; j++) {
+      flag = flag && objArray[j][char] !== undefined;
+      console.log(char + flag);
+    }
+    if (flag === true && result.indexOf(char) === -1) {
       result.push(char);
     }
   }
-  return result.join('');
+  return result;
 };
 
-//test
 var result = commonCharacters('aeexivou', 'aegeihobu');
 console.log(result);
