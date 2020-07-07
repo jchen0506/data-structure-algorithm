7baa32e27047ec22d32a4149e344e7805ed9cce9
Jing Chen <jchen0506@gmail.com>
2020-07-07 11:25:55 -0500

    first attempt for largestProduct

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,35 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
-var largestProductOfThree = function(array) {
+var largestProductOfThree = function (array) {
   // TODO: everything
+  //find the largest 3 number in the array.
+  /*   [5,3,1]->[5,3,4]->[5,9,4]->[5,9,7]->[6,9,7]
+    push 3 number into array and sort it
+    if next number is larger than min of array
+    push new number ,pop min
+    return product of the 3 numbers array
+    */
+  var result = [];
+  for (var i = 0; i < 3; i++) {
+    result.push(array[i]);
+  }
+  // result.sort(comparator);
+  for (var j = 3; j < array.length; j++) {
+    var min = result.sort(comparator)[2];
+    if (array[j] > min) {
+      result.pop();
+      result.push(array[j]);
+    }
+  }
+  var product = result[0] * result[1] * result[2];
+  return product;
 };
+
+var comparator = function (a, b) {
+  return b - a;
+};
+
+var array = [5, 3, 1, 4, 9, 7, 6];
+
+console.log(largestProductOfThree(array)); //expect (9*7*6)=378
