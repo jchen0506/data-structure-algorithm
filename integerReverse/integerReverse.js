0a74d0522fb93d18cd2c1819bf2864616cbbbdc6
Jing Chen <jchen0506@gmail.com>
2020-07-10 12:03:16 -0500

    find the 0 edge case

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -15,6 +15,10 @@ function reverseInteger(number) {
   var reversed = 0;
   var digit = number % 10;
 
+  if (number === 0) {
+    return 0;
+  }
+
   while (digit === 0) {
     number = Math.floor(number / 10);
     digit = number % 10;
@@ -28,11 +32,20 @@ function reverseInteger(number) {
   return reversed;
 }
 
-console.log(reverseInteger(1001000)); // 379475
-console.log(reverseInteger(379475)); // 574973
+console.log(reverseInteger(100)); // 379475
+console.log(reverseInteger(1234)); // 574973
+console.log(reverseInteger(150)); // 574973
+console.log(reverseInteger(2342617432)); // 574973
+console.log(reverseInteger(2836591)); // 574973
+console.log(reverseInteger(58648550)); // 574973
+console.log(reverseInteger(5748104710)); // 574973
+console.log(reverseInteger(77477477)); // 574973
+console.log(reverseInteger(1)); // 574973
+console.log(reverseInteger(10)); // 574973
+console.log(reverseInteger(0)); // 574973
 
 /*
-  574973 %10 =3 (574972 / 10)  => 3*1
+574973 %10 =3 (574972 / 10)  => 3*1
    57497 %10 =7 => 3*10+7=37
     5749 %10 =9 => 3*10*10+7*10+9*1=379
     574  %10 =4  => 3*1000+7*100+9*10 +4 =3794
