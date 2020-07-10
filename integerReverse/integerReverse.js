61597c288776084f7ca4aeb8f10a180b2f726b75
Jing Chen <jchen0506@gmail.com>
2020-07-10 11:21:18 -0500

    finsihed integerReverse normal case

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,37 @@
-/* 
-* 
-* Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
-* - Only use integers and math in your solution.
-*
-*/
+/*
+ *
+ * Integer Reverse
+ *
+ * Given a positive integer, return its digits reversed.
+ *
+ * - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
+ * - Only use integers and math in your solution.
+ *
+ */
 
-function reverseInteger(number){
+function reverseInteger(number) {
   // TODO: Implement this function!
+
+  var i = 10;
+  var reversed = 0;
+  while (number % i > 0) {
+    var digit = number % 10;
+    console.log(digit);
+    number = Math.floor(number / 10);
+    console.log(number);
+    reversed = reversed * 10 + digit;
+    console.log(digit);
+  }
+  return reversed;
 }
+
+console.log(reverseInteger(574973)); // 379475
+
+/*
+  574973 %10 =3 (574972 / 10)  => 3*1
+   57497 %10 =7 => 3*10+7=37
+    5749 %10 =9 => 3*10*10+7*10+9*1=379
+    574  %10 =4  => 3*1000+7*100+9*10 +4 =3794
+    57   %10 =7  => 3784*10+7=37947
+    5    %10 =0 => 37947*10+5= 379475
+*/
