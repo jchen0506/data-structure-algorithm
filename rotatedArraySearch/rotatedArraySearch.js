5b898a0c1394565e861ee90c801d6b32c8b32bdc
Jing Chen <jchen0506@gmail.com>
2020-07-18 11:55:18 -0500

    solved but the O(nlgn)

--- rotatedArraySearch/rotatedArraySearch.js
+++ rotatedArraySearch/rotatedArraySearch.js
@@ -17,5 +17,32 @@
 
 var rotatedArraySearch = function (rotated, target) {
   // Your code here:
+  var resort = rotated.sort();
+  console.log(resort);
+  var left = 0;
+  var right = resort.length - 1;
+  while (left <= right) {
+    var mid = Math.floor((left + right) / 2);
+    if (resort[mid] === target) {
+      return mid;
+    } else {
+      if (target < resort[mid]) {
+        right = mid - 1;
+        console.log(right);
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return -1;
 };
 
+console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100));
+/*
+[4,5,6,0,1,2,3]
+[2,3,4,5,6,0,1]
+index 0-6, 7=0
+6 0 1 2 3 4 5
+2, 2>1, 2<3,
+5, 5<6, 5>4
+*/
