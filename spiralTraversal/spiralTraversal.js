f8ce510394c19b9782c15c6316b6195f08be02d6
Jing Chen <jchen0506@gmail.com>
2020-07-21 11:58:16 -0500

    attempted on spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -14,7 +14,60 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix) {
-
+var spiralTraversal = function (matrix) {
   // TODO: Implement me!
+  var result = [];
+  var flag = [];
+  for (let row = 0; row < matrix.length; row++) {
+    var flagrow = [];
+    for (let column = 0; column < matrix[0].length; column++) {
+      flagrow.push(false);
+    }
+    flag.push(flagrow);
+  }
+  var i = 0;
+  var j = 0;
+  while (flag[i][j] === false || matrix[i][j] !== undefined) {
+    result.push(matrix[i][j]);
+    j++;
+    flag[i][j] = true;
+  }
+  i++;
+  j--;
+  while (flag[i][j] === false || matrix[i][j] !== undefined) {
+    result.push(matrix[i][j]);
+    i++;
+    flag[i][j] = true;
+  }
+  j--;
+  i--;
+  while (flag[i][j] === false || matrix[i][j] !== undefined) {
+    result.push(matrix[i][j]);
+    j--;
+    flag[i][j] = true;
+  }
+  i--;
+  while (flag[i][j] === false || matrix[i][j] !== undefined) {
+    result.push(matrix[i][j]);
+    i--;
+    flag[i][j] = true;
+  }
+
+  return result;
 };
+
+//test case
+console.log(
+  spiralTraversal([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+
+/*
+ row stays, column+1, until visited or undefined
+ row+1, column stays, until visited or undefined
+ row stays, column-1, until visited or undefined
+ row-1, column statys, until visited or undefined
+ */
