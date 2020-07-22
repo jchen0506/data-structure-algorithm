e856940502b0b2fa150977c405a4304153fa5051
Jing Chen <jchen0506@gmail.com>
2020-07-22 11:55:48 -0500

    finished edit distance

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -4,4 +4,53 @@
 
 function editDistance(str1, str2) {
   // Your code here.
+  var arr1 = str1.split('');
+  var arr2 = str2.split('');
+  var m = str1.length;
+  var n = str2.length;
+  if (m * n === 0) {
+    return m + n;
+  }
+  var dp = [];
+  dp[0] = [];
+  for (let j = 0; j < n + 1; j++) {
+    dp[0].push(j);
+  }
+  for (let i = 1; i < m + 1; i++) {
+    dp.push([i]);
+  }
+
+  for (let i = 1; i < m + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      if (arr1[i - 1] !== arr2[j - 1]) {
+        dp[i].push(1 + Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]));
+      } else {
+        dp[i].push(dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
 }
+
+console.log(editDistance('abcde', 'ebad'));
+/*
+  abcde
+  ebad
+
+  [e,b,d]
+  [a,b,c,d,e]
+  a-e, a-b,a-d, +1
+  b-e, b-b,
+  c-d, +1
+  d-d,
+  e-undefined +1
+  1. e->a
+  2. insert c
+  3. insert e
+
+  [a,b,c,d,e]
+  [e,b,d]
+  e-a,e-b,e-c,e
+  [null,b,d]
+ */
