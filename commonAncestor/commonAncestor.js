980854de99e6eba63530cae769a2a44e764cea58
Jing Chen <jchen0506@gmail.com>
2020-07-23 11:40:28 -0500

    attempt commonAncestor

--- commonAncestor/commonAncestor.js
+++ commonAncestor/commonAncestor.js
@@ -1,26 +1,25 @@
-
 /**
-  * implement the function `getClosestCommonAncestor` and `getAncestorPath`
-  * in the following Tree class
-  */
+ * implement the function `getClosestCommonAncestor` and `getAncestorPath`
+ * in the following Tree class
+ */
 
 /** example usage:
-  * var grandma = new Tree();
-  * var mom = new Tree();
-  * grandma.addChild(mom);
-  * var me = new Tree();
-  * mom.addChild(me);
-  * grandma.getAncestorPath(me); // => [grandma, mom, me]
-*/
+ * var grandma = new Tree();
+ * var mom = new Tree();
+ * grandma.addChild(mom);
+ * var me = new Tree();
+ * mom.addChild(me);
+ * grandma.getAncestorPath(me); // => [grandma, mom, me]
+ */
 
-var Tree = function() {
+var Tree = function () {
   this.children = [];
 };
 
 /**
-  * add an immediate child
-  */
-Tree.prototype.addChild = function(child) {
+ * add an immediate child
+ */
+Tree.prototype.addChild = function (child) {
   if (!this.isDescendant(child)) {
     this.children.push(child);
   } else {
@@ -30,37 +29,47 @@ Tree.prototype.addChild = function(child) {
 };
 
 /**
-  * return the lowest common ancestor of the two child nodes.
-  * (assume for these examples that only a women can be the parent of a child)
-  * more examples:
-  *  1.) between me and my brother -> my mom
-  *  2.) between me and my cousin -> my grandma
-  *  3.) between my grandma and my grandma -> my grandma
-  *  4.) between me and a potato -> null
-  */
-Tree.prototype.getClosestCommonAncestor = function(/*...*/
-) {
+ * return the lowest common ancestor of the two child nodes.
+ * (assume for these examples that only a women can be the parent of a child)
+ * more examples:
+ *  1.) between me and my brother -> my mom
+ *  2.) between me and my cousin -> my grandma
+ *  3.) between my grandma and my grandma -> my grandma
+ *  4.) between me and a potato -> null
+ */
+Tree.prototype.getClosestCommonAncestor = function () /*...*/
+{
   // TODO: implement me!
 };
 
 /**
-  * should return the ancestral path of a child to this node.
-  * more examples:
-  * 1.) greatGrandma.getAncestorPath(me) -> [great grandma, grandma, mom, me]
-  * 2.) mom.getAncestorPath(me) -> [mom, me]
-  * 3.) me.getAncestorPath(me) -> [me]
-  * 4.) grandma.getAncestorPath(H R Giger) -> null
-  */
-Tree.prototype.getAncestorPath = function(/*...*/
-) {
+ * should return the ancestral path of a child to this node.
+ * more examples:
+ * 1.) greatGrandma.getAncestorPath(me) -> [great grandma, grandma, mom, me]
+ * 2.) mom.getAncestorPath(me) -> [mom, me]
+ * 3.) me.getAncestorPath(me) -> [me]
+ * 4.) grandma.getAncestorPath(H R Giger) -> null
+ */
+Tree.prototype.getAncestorPath = function (child /*...*/) {
   // TODO: implement me!
+  var result = [];
+  if (!this.isDescendant(child)) return null;
+  else {
+    result.push(this);
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].isDescendant(child)) {
+        this.children[i].getAncestorPath(child);
+      }
+    }
+  }
+  return result;
 };
 
 /**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-Tree.prototype.isDescendant = function(child) {
+ * check to see if the provided tree is already a child of this
+ * tree __or any of its sub trees__
+ */
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -76,9 +85,9 @@ Tree.prototype.isDescendant = function(child) {
 };
 
 /**
-  * remove an immediate child
-  */
-Tree.prototype.removeChild = function(child) {
+ * remove an immediate child
+ */
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
@@ -87,3 +96,19 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+//example usage:
+var grandma = new Tree();
+var mom = new Tree();
+var greatGrandma = new Tree();
+greatGrandma.addChild(grandma);
+grandma.addChild(mom);
+var me = new Tree();
+mom.addChild(me);
+grandma.getAncestorPath(me); // => [grandma, mom, me]
+
+var other = new Tree();
+console.log(greatGrandma.getAncestorPath(me)); //[great grandma, grandma, mom, me]
+mom.getAncestorPath(me); //-> [mom, me]
+me.getAncestorPath(me); // -> [me]
+console.log(grandma.getAncestorPath(other)); // -> null
